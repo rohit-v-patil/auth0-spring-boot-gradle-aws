84b92ba2be6877cceaceac2f72cf8ba42ffbd336
rohit1727 <rprohit72@gmail.com>
2022-07-23 14:42:55 +0530

    removed redundant tests

--- src/test/java/com/app/testProject/TestProjectApplicationTests.java
+++ src/test/java/com/app/testProject/TestProjectApplicationTests.java
@@ -1,13 +1,8 @@
 package com.app.testProject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
